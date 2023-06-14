a6b6c1a9bf9cb197a5c34172e7ba6839c64854ec
sallproject <mhmmd.faisal.mf.2123@gmail.com>
2023-06-14 23:34:41 +0800

    Menjalankan program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -2,6 +2,11 @@ package universitas;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
+        // Membuat objek Mahasiswa dengan nama sendiri
+       Mahasiswa mhs = new Mahasiswa("2110010467", "Muhammad Faisal", "Teknik Informatika");
         
+        // Memanggil method getDetail() dan mencetak hasilnya
+        String detail = mhs.getDetail();
+        System.out.println(detail);
     }
 }
